89e6b8258c9236a6372780592604ec8b670139b8
Default <17170569@itculiacan.edu.mx>
2024-11-23 23:38:39 -0700

    Backend

---
+++ Backend/BibliotecaBackend/BibliotecaBackend/Models/Libro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BibliotecaBackend.Models
+{
+	public class Libro
+	{		
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
+
---
+++ Backend/BibliotecaBackend/BibliotecaBackend/Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Obtener el puerto de la variable de entorno
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";  // Usar 5000 por defecto si no se encuentra PORT
+
+// Configura Kestrel para usar el puerto dinámico
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(int.Parse(port)); // Escuchar en el puerto proporcionado
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
